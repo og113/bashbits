3acd4de112ca62ecb7ec6427ed3071d276356026
Gould Oliver L <olivergo@lx8-915-001.ad.helsinki.fi>
2019-01-29 22:50:06 +0200

    fixed trim

--- trim.sh
+++ trim.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-then
-	echo "must supply input file"
-	exit 1
-fi
+h=''
+t=''
+totrim=1
+f=''
+
+print_usage() {
+  printf "usage: trim -t -n 1 -f file"
+}
+
+while getopts 'htn:f:' flag; do
+  case "${flag}" in
+    h) h='true' ;;
+    t) t='true' ;;
+    n) totrim=${OPTARG} ;;
+    f) f=${OPTARG} ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
 
 
-if [ $# -eq 1 ]
+if [ -z $f ]
 then
-	f=$1
-	totrim=1
-elif
-then
-	totrim=$1
-	f=$2
+	print_usage
 fi
 
 len=$(cat $f | wc -l)
-len_trimmed=$(bc <<< "$len - $totrim")d
-head -n $len_trimmed $f
+len_trimmed=$(bc <<< "$len - $totrim")
+
+if [ -z $t ]
+then
+	head -n $len_trimmed $f
+else
+    tail -n $len_trimmed $f
+fi
+
